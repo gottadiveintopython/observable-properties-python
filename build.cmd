92c19d9e42891c3921a1fade0b4949b3003556e4
Nattōsai Mitō <flow4re2c@gmail.com>
2024-08-15 08:03:08 +0900

    poetryへの対応

--- build.cmd
+++
@@ -1,8 +0,0 @@
-@echo observable_properties: build for distribution
-@echo ---------------------------------
-@echo Running build frontend:
-del dist\*.gz
-del dist\*.whl
-python -m build
-@echo Install in editable mode:
-pip install -e .
--- test-deploy.cmd
+++
@@ -1,4 +0,0 @@
-@echo observable_properties: deploying to test.pypi.org
-@echo ---------------------------------
-twine check dist/*
-twine upload -r testpypi dist/*
